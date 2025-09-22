e36528a8a95bdf0c4c363ac66507faf4dde04ccb
jannkobee <jkobe415@gmail.com>
2025-09-22 18:23:13 +0800

    fix: change api and non api in develop

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,10 +3,10 @@ import { notification } from "antd";
 import { Capacitor } from "@capacitor/core";
 
 // 👇 choose base URL depending on environment
-// const api = "http://127.0.0.1:8000/backend/api/v1";
-// const nonApi = "http://127.0.0.1:8000/storage";
-const api = "https://bibo-ai-backend.onrender.com/backend/api/v1";
-const nonApi = "https://bibo-ai-backend.onrender.com/storage";
+const api = "http://127.0.0.1:8000/backend/api/v1";
+const nonApi = "http://127.0.0.1:8000/storage";
+// const api = "https://bibo-ai-backend.onrender.com/backend/api/v1";
+// const nonApi = "https://bibo-ai-backend.onrender.com/storage";
 
 // Configure Ant Design notification globally
 notification.config({
